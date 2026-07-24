7ded0de8f078f23417744e3ceeb39704745b5c1b
ANA CLARA RODRIGUES ABREU <ana_cr_abreu@estudante.sesisenai.org.br>
2026-07-23 21:34:17 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from models.funcionario import funcionario
+
+funcionario1 = funcionario
+("carlos Silva", "11111111111", "operador CNC" , 4500)
+
+print(funcionario1.nome)
+print(funcionario1.cpf)
+print(funcionario1.cargo)
+print(funcionario1.salario)
+
+
+funcionario2 = funcionario
+("Joana Pereira", "11111111133", "Supervisora" , 5500)
+
+print(funcionario2.nome)
+print(funcionario2.cpf)
+print(funcionario2.cargo)
+print(funcionario2.salario)
