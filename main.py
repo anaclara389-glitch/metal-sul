06760d2c4ce3949c7f5187e1620627cd8107abe1
anaclara389-glitch <abreuanaclara389@gmail.com>
2026-07-29 14:59:10 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 from models.funcionario import funcionario
 
-funcionario1 = funcionario
-("carlos Silva", "11111111111", "operador CNC" , 4500)
-
-print(funcionario1.nome)
-print(funcionario1.cpf)
-print(funcionario1.cargo)
-print(funcionario1.salario)
+funcionario1 = funcionario("Douglas", "123.456.789-00", "Engenheiro", 5000.00)
+funcionario2 = funcionario("Richard", "123.426.779-00", "Técnico", 4000.00)
+funcionario3 = funcionario("Ryan", "223.456.789-10", "ADM", 5000.00)
+funcionario4 = funcionario("Wesley", "123.426.709-00", "Engenheiro Elétrico", 10000.00)
 
 
-funcionario2 = funcionario
-("Joana Pereira", "11111111133", "Supervisora" , 5500)
 
-print(funcionario2.nome)
-print(funcionario2.cpf)
-print(funcionario2.cargo)
-print(funcionario2.salario)
+
+print(f"Nome: {funcionario1.nome}")
+print(f"CPF: {funcionario1.cpf}")
+print(f"Cargo: {funcionario1.cargo}")
+print(f"Salário: {funcionario1.salario}")
+
+print("-"*50)
+print(f"Nome: {funcionario2.nome}")
+print(f"CPF: {funcionario2.cpf}")
+print(f"Cargo: {funcionario2.cargo}")
+print(f"Salário: {funcionario2.salario}")
+
+print("-"*50)
+print(f"Nome: {funcionario3.nome}")
+print(f"CPF: {funcionario3.cpf}")
+print(f"Cargo: {funcionario3.cargo}")
+print(f"Salário: {funcionario3.salario}")
+
+print("-"*50)
+print(f"Nome: {funcionario4.nome}")
+print(f"CPF: {funcionario4.cpf}")
+print(f"Cargo: {funcionario4.cargo}")
+print(f"Salário: {funcionario4.salario}")
