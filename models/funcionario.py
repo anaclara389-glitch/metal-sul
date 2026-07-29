9ddacfc24330765f86b05052927a678bba3ad798
anaclara389-glitch <abreuanaclara389@gmail.com>
2026-07-29 14:59:46 -0300

    Update funcionario.py

--- models/funcionario.py
+++ models/funcionario.py
@@ -1,5 +1,5 @@
 class funcionario:
-        def init(self, nome, cpf, cargo, salario):
+        def __init__(self, nome, cpf, cargo, salario):
             self.nome = nome
             self.cpf = cpf
             self.cargo = cargo
